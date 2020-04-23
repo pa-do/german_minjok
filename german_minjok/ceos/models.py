02800b765396f9a1d4ec7258b0d6b7ceaa7218c4
jokyuseong <pandora_h@naver.com>
2020-04-23 08:23:44 +0000

    Update| ceos models.py| change on_delete

--- german_minjok/ceos/models.py
+++ german_minjok/ceos/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+def get_sentinel_user():
+    return get_user_model().objects.get_or_create(username='deleted')[0]
+
+def get_sentinel_store():
+    return StoreInfo.objects.get_or_create(store_name='deleted')[0]
+
 
 class StoreInfo(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -12,7 +19,6 @@ class StoreInfo(models.Model):
 
 
 class StoreMenu(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     store = models.ForeignKey(StoreInfo, on_delete=models.CASCADE)
     menu_name = models.CharField(max_length=50)
     menu_price = models.IntegerField()
@@ -20,7 +26,10 @@ class StoreMenu(models.Model):
 
 
 class OrderList(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    store = models.ForeignKey(StoreInfo, on_delete=models.CASCADE)
-    menu = models.ForeignKey(StoreMenu, on_delete=models.CASCADE)
-    order_status = models.IntegerField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET(get_sentinel_user))
+    store = models.ForeignKey(StoreInfo, on_delete=models.SET(get_sentinel_store))
+    order_condition = models.IntegerField(default=0, choices=[(0, "신청"), (1, "준비"), (2, "완료")])
+    order_location =  models.CharField(max_length=100)
+    order_time = models.DateTimeField(auto_now_add=True)
+    order_name = models.CharField(max_length=50)
+    order_price = models.IntegerField()
