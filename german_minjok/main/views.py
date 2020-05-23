c0f1d8e46eb853484a0e7cdb6df376873240bc53
jokyuseong <pandora_h@naver.com>
2020-05-23 22:52:28 +0900

    CREATE | Cart, edit carton

--- german_minjok/main/views.py
+++ german_minjok/main/views.py
@@ -1,18 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
 
-from ceos.models import Store
 # Create your views here.
 
 def index(request):
     return render(request, 'main/index.html')
-
-
-def menu(request, store_pk):
-    store = get_object_or_404(Store, pk=store_pk)
-    menu_list = store.storemenu_set.all()
-    context = {
-        'menue_list': menu_list,
-        'store': store,
-    }
-    return render(request, 'main/menu_list.html')
